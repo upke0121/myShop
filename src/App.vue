7f3ebc03021db5542b7dfd29385c38ac3aa30704
upke0121 <leidengke0121@163.com>
2021-04-30 09:47:29 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view />
+    <FooterGuide v-show="$route.meta.isShowFooter"/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    name: 'App',
+
+    async mounted () {
+      this.$store.dispatch('getAddress')
+      this.$store.dispatch('autoLogin')
+    },
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
